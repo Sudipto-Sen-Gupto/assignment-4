5b12138474fba11b7862e853ba6099080d8bc13e
Sudipta Sen Gupta <gauravsengupto321@gmail.com>
2025-08-13 11:31:11 -0500

    fifa fare award

---
+++ fifaAward.js
@@ -0,0 +1,25 @@
+ 
+ function  bestTeam( player1, player2 ) {
+          // You have to write your code here
+          if(typeof player1 !=="object" || typeof player2 !=="object"){
+            return "Invalid"
+          }
+          
+          let firstTeam=player1.foul+player1.cardY+player1.cardR;
+          let secondTeam=player2.foul+player2.cardY+player2.cardR;
+
+          if(firstTeam<secondTeam){
+            return player1.name;
+          }
+          else if(firstTeam>secondTeam){
+            return player2.name;
+          }
+          else{
+            return "tie";
+          }       
+}
+
+const khe=bestTeam({ name: "Germany", foul: 10, cardY: 1, cardR: 1 },"France"
+
+)
+console.log(khe);
---
+++ final-paper.js
@@ -0,0 +1,10 @@
+function totalFine( fare ) {
+        // You have to write your code here
+        if(typeof fare !=="number" || fare<=0){
+            return "Invalid";
+        }
+        let number=fare+(fare*20/100)+30;
+        return number;
+     }
+
+ 
